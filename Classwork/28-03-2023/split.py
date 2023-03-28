d238eb52fc9b0ae59e35f40c21eb31aef7e4ead4
Parshwa Bhavsar <bhavsarparshwa@gmail.com>
2023-03-28 15:44:35 +0530

    p

---
+++ Classwork/28-03-2023/split.py
@@ -0,0 +1,9 @@
+# mystr = "Hello this is Python"
+
+# print(mystr)
+# print(mystr.split())                            # to split the words of the string
+
+fnm, lnm = input("Enter Your First name and Last name : ").split()
+# print("First Name : ",fnm)
+# print("Last Name : ",lnm)
+
