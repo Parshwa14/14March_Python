45235b9899b6908bbae92fd172ec23a8102ff025
Parshwa Bhavsar <bhavsarparshwa@gmail.com>
2023-04-26 16:15:44 +0530

    p

--- Assessment/Core Python Assessment/Fruit Market Management.py
+++ Assessment/Core Python Assessment/Fruit Market Management.py
@@ -1,49 +1,91 @@
-# displaying the app/program name
-print("\t\tWELCOME TO FRUIT MARKET\n")
+import fm_manage
+# create an empty dictionary
+fruit_list = {}
+# status for main menu repeatation and f_status for manager menu repeatation
+status = True
+f_status = True
 
-# creating a function for main menu
-def mmenu():
-    return print("\t\t1) Manager\n \t\t2) Customer\n")
+# printing the welcome note
+print("\n\t\tWelcome To Fruit Market\n\n")
 
-# creating a function for menu
-def menu():
-    return print("\t\tFruit Market Manager\n\n \t\t1) Add Fruit Stock\n \t\t2) View Fruit Stock\n \t\t3) Update Fruit Stock")
-
-# calling main menu
-mmenu()     
-# created a variable role for taking an input from user 
-role = int(input("\tSelect Your Role : "))
-
-# for 1 it will take to manager's roles
-if role == 1:
+while status:
+    # printing main menu for choosing our choice from the module
+    x=fm_manage.main_menu()
+    print(x)
     
-    # in manager's role there is 3 options
-    # showing all 3 option with the help of menu funtion
-    menu()
+    # using role variable to select the role manager or customer 
+    role = int(input("\tSelect Your Role : "))
 
-    # making user chose from the menu 
-    choice = int(input("\tSelect Your Choice : "))
+    # for the manager section
+    if role == 1:
 
-    # for 1 user can add fruit stock
-    if choice == 1:
-        print("\n\t\tYou have selected to add fruit stock")
+        # printing the manager menu from the module
+        y=fm_manage.menu()
+        
+        # f_status with while to repeat the fruit market manager menu
+        while f_status:
+            print(y)
 
-    # for 2 user can view fruit stock
-    elif choice == 2:
-        print("\n\t\tYou have selected to view fruit stock")
+            # choice variable for selecting from add,view,update
+            choice = int(input("\tEnter Your Choice : "))
+            
+            #creating a dictionary in the main dictionary
+            spec={}
+        
+            # for adding the fruit stock
+            if choice == 1 :                                
+                fruit_name=input("Enter Fruit Name : ")         # asking for fruit name
+                fruit_qty = int(input("Enter Fruit Quantity (in kgs): "))           # asking for quantity of fruit
+                amount = int(input("Enter Total Amount : "))            # asking for the total amount (amount per kg x quantity)
+                
+                # adding fruit stock to the dictionary 
+                spec["fruit_qty"]=fruit_qty
+                spec["amount"]=amount
+                fruit_list[fruit_name]=spec
+
+                # showing message after adding the stock successfully 
+                print("Successfully added fruit stock")
+                
+            # for viewing the fruit stock
+            elif choice == 2 :
+                print(fruit_list)
+            
+            # for updating the fruit stock
+            elif choice == 3 :
+
+                # asking user for the fruit name,quantity and price
+                fruit_name=input("Enter Fruit Name : ")
+                fruit_qty = int(input("Enter Fruit Quantity (in kgs): "))
+                # amount = int(input("Enter Total Amount : "))
+                
+                # if the fruit name is already in the dictionary
+                if fruit_name in fruit_list.keys():
+                    
+                    # updating the quantity and price
+                    spec["fruit_qty"] = fruit_list[fruit_name]["fruit_qty"] + fruit_qty
+                    spec["amount"] = amount
+
+                    # showing message for successful updation 
+                    print("Successfully updated fruit stock")
+            
+            else:
+                print("Invalid Choice !!!")
+
+            # for the while loop in the fruit menu 
+            f_choice =input("Do you want to add or view or update stock ? y/n : ")
+            if f_choice == "n" or f_choice == "N":
+                f_status = False
+
+    # the customer section 
+    elif role == 2:
+        print("\t\t :: Buy Fresh Fruits Here :: ")
+        
+    # for exit using the status variable and declaring it False to exit the loop and program
+    elif role == 3:
+        print("Thank you for comming to Out Store!!")
+        status = False    
     
-    # for 3 user can update fruit stock
-    elif choice == 3:
-        print("\n\t\tYou have selected to update fruit stock")
-    
-    # for any other input it will show an error    
+    # for invalid input 
     else:
-        print("\n\t\tInvalid Choice!")
-
-# for 2 it will take to customer's role
-elif role == 2:
-    print("\n\t\tYou have select customer role")
-
-# for any other input it will show an error
-else:
-    print("\n\t\tInvalid Role!!")
+        print("Invalid Choice !!!")
+        
---
+++ Assessment/Core Python Assessment/fm_manage.py
@@ -0,0 +1,6 @@
+def main_menu():
+    return "\t\t1) Manager\n \t\t2) Customer\n \t\t3) exit\n"
+    
+
+def menu():
+    return "\n\t\tFruit Market Manager\n\n \t\t1) Add Fruit Stock\n \t\t2) View Fruit Stock\n \t\t3) Update Fruit Stock"
